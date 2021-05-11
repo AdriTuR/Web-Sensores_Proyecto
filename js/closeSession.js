a952ec34c2298c6956af7f0d7ee426b4a938df36
Victor Gracia <vgaresp@epsg.upv.es>
2021-05-11 19:03:49 +0200

    conect bbdd, footer&header new, login +
    El esto de cosas es que la he liado un poco, por si acaso, haced una copia local de proyecto y después el pull

--- js/closeSession.js
+++ js/closeSession.js
@@ -1,9 +1,9 @@
 function disconnect(){
-    fetch("/api/v1/", {
+    fetch("./api/v1/", {
         method: "DELETE"
     }).then(function (result) {
         if(result.status == 200){
-            location.href = "/login.html";
+            location.href = "./login.php";
         }
     }); 
 }
