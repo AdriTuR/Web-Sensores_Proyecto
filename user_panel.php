2932a056e34df5f4baa1b1b7fca72a42515dba1d
Sairo1234 <alejandronogbel@gmail.com>
2021-05-13 22:20:13 +0200

    Panelsito de usuario

--- user_panel.php
+++ user_panel.php
@@ -1,35 +1,92 @@
 <?php
-    $t = 1;
+    $t = 2;
     $name = "Panel Usuario";
     include_once './includes/header.php';
     function customHead(){?>
-        <!-- <link rel="stylesheet" href="./css/adminp-style.css"> --->
-    <?php }
+        <script src="https://maps.googleapis.com/maps/api/js" async defer></script>
+        <link rel="stylesheet" href="./css/userp-style.css">
+        <style>
+            #map {
+                height: 70vh;
+                border: solid 1px black;
+            }
+        </style>
+<?php }
 ?>
 <!-------------------------------------------------------------------------------------------------------------------->
 <!--------------------------------------------- BODY DE LA PÁGINA ---------------------------------------------------->
 <!-------------------------------------------------------------------------------------------------------------------->
+<body>
+    <input type="checkbox" id="check">
+    <label for="check">
+        <i class="fas fa-bars menuIcon" id="btn"></i>
+        <i class="fas fa-times menuIcon" id="cancel"></i>
+    </label>
 
+    <nav class="sidebar">
+    <header> PANEL DE USUARIO </header>    
+    <hr class="line">
+        <ul>
+            <li><a href="#"><i class="fas fa-map-marked-alt"></i>TERRENO</a></li>
+            <li><a href="#"><i class="fas fa-chart-line"></i>GRÁFICAS</a></li>
+            <li><a href="#"><i class="fas fa-user"></i>PERFIL</a></li>
+            <li><a class="btn_logout" type="button" onclick="disconnect()"><i class="fas fa-sign-out-alt"></i>CERRAR SESIÓN</a></li>
+        </ul>
+        <br>
+        <hr class="line">
+        <footer> <i class="fas fa-user-circle"></i> <span class="usuario"> Joseba Jimenez </span></footer>
+        
+    </nav>
     <h1>Bienvenido usuario</h1>
-    <button class="btn_logout" type="button" onclick="disconnect()">Cerrar sesión</button>
-    <script>
-        window.addEventListener("load", function(){
-            fetch("/api/v1/", {
-                method: "GET"
-            }).then(function (result) {
-                if(result.status == 200){
-                    return result.json();
-                }else{
-                    location.href = "./login.html";
-                }  
-            }).then(function (data) {
-                /*
-                if(data.role == "ADMIN"){
-                    location.href = "/admin_panel.html";
-                }
-                */
-            }); 
+    
+    <div id="map"></div>
+
+    <!------------------------------- FIN BODY DE LA PÁGINA ---------------------------------------->
+
+    <!-------------------------------- FOOTER DE LA PÁGINA ------------------------------------------>
+    <?php include_once './includes/footer.php';?>
+    <!------------------------------- FIN FOOTER DE LA PÁGINA ---------------------------------------->
+
+<script>
+    window.addEventListener("load", function(){
+        fetch("/api/v1/", {
+            method: "GET"
+        }).then(function (result) {
+            if(result.status == 200){
+                return result.json();
+            }else{
+                location.href = "./login.html";
+            }  
+        }).then(async function (data) {
+            await initMap();
+            addCustomerMap(data.name);
+            /*
+            if(data.role == "ADMIN"){
+                location.href = "/admin_panel.html";
+            }
+            */
+        }); 
+    });
+
+    function addCustomerMap(username){
+        var formData = new FormData();
+        formData.append('user', username);
+
+        fetch("./api/v1/probes.php", {
+            method: "POST",
+            body: formData
+        }).then(function (result) {
+            if(result.status == 200){
+                return result.json();
+            }
+        }).then(async function (data) {
+            if(data != null){
+                var json = JSON.parse(JSON.stringify(data));
+                showUserMap(parseJSONLocationFormat(json.fieldData), parseJSONLocationFormat(json.plotData), parseJSONLocationFormat(json.probeData));
+            }
         });
-    </script>
-    <script src="./js/closeSession.js"></script>
+    }
+</script>
+<script src="./js/closeSession.js"></script>
+<script src="./js/map.js"></script>
 </html>
