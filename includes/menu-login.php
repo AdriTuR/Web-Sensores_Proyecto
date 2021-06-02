89ccf96b12752ed8c13a37e2cdf66ba0da248ff4
Victor Gracia <vgaresp@epsg.upv.es>
2021-06-02 21:04:10 +0200

    login bien(falta mover cosas al common)

--- includes/menu-login.php
+++ includes/menu-login.php
@@ -3,10 +3,10 @@
     <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
 
     <ul class="menu">
-        <li onclick="menucerrar()"><a href="./index.php#mas_informacion">Qué te ofrecemos</a></li>
-        <li onclick="menucerrar()"><a href="./index.php#informacion_web">Más información</a></li>
-        <li onclick="menucerrar()"><a href="./index.php#acerca_de_nosotros">Acerca de nosotros</a></li>
-        <li onclick="menucerrar()"><a href="index.php">Volver</a></li>
+        <li onclick="menucerrar()"><div class="menu_icon"><i class="fas fa-hand-holding"></i><a href="./index.php#mas_informacion">Qué te ofrecemos</a></div></li>
+        <li onclick="menucerrar()"><div class="menu_icon"><i class="fas fa-info-circle"></i><a href="./index.php#informacion_web">Más información</a></div></li>
+        <li onclick="menucerrar()"><div class="menu_icon"><i class="fas fa-users"></i><a href="./index.php#acerca_de_nosotros">Acerca de nosotros</a></div></li>
+        <li onclick="menucerrar()"><div class="menu_icon"><a href="index.php">Volver</a></div></li>
     </ul>
 </nav>
 
