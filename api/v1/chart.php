1a9f12ccc80462b1778e93f8d1d20817baf3d5a2
KB <krystian8844@gmail.com>
2021-06-12 16:33:30 +0200

    Funcionalidad de gráficos de sondas

--- api/v1/chart.php
+++ api/v1/chart.php
@@ -2,35 +2,53 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-if ($method === 'POST'){
+if ($method === 'GET'){
+  
+  require_once '../includes/connection.php';
+  
+  $probeID = trim($_GET['probeID']);
+  $probeID = 1;
 
-    require_once '../includes/connection.php';
+  $sql = "SELECT *  FROM `probe_history` WHERE `probeID` = '$probeID' ORDER by date;";
+  
+  $dateArray = array();
+  if(isset($_GET['date'])){
+    $date = trim($_GET['date']);
+    $sql = "SELECT * FROM `probe_history` WHERE `probeID` = '$probeID' AND `date` = '$date';";
 
-    $probeID = trim($_POST['probeID']);
-
-    $sql = "SELECT *  FROM `probe_history` WHERE `probeID` = '$probeID';";
-    $result = mysqli_query($conn, $sql);
-
-    $plotData = array();
-
-    if(mysqli_num_rows($result) > 0){
-        $row = mysqli_fetch_assoc($result);
-        class Plot {
-            public $h;
-            public $t; 
-            public $s; 
-            public $date; 
-            
-            function __construct($a, $b, $c, $d) {
-              $this->h = $a;
-              $this->t = $b;
-              $this->s = $c;
-              $this->date = $d;
-            }
-          }
-
-          $plot = new Plot($row["humidity"], $row["temperature"], $row["salinity"], $row["date"]);
-   
-          echo json_encode($plot);         
-}
+  }else {
+    $sql2 = "SELECT date FROM `probe_history` WHERE `probeID` = '$probeID'";
+    $result = mysqli_query($conn, $sql2);
+    
+    if(!empty($result) && mysqli_num_rows($result) > 0){
+      while($row = mysqli_fetch_array($result)) {
+        array_push($dateArray, $row['date']);
+      }
+    }
+  }
+  
+  $result = mysqli_query($conn, $sql);
+  
+  if(mysqli_num_rows($result) > 0){
+    $row = mysqli_fetch_assoc($result);
+    
+    class Data {
+      public $h;
+      public $t; 
+      public $s; 
+      public $date; 
+      public $allDates;
+      
+      function __construct($a, $b, $c, $d, $e) {
+        $this->h = $a;
+        $this->t = $b;
+        $this->s = $c;
+        $this->date = $d;
+        $this->allDates = $e;
+      }
+    }
+    
+    $data = new Data($row["humidity"], $row["temperature"], $row["salinity"], $row["date"], $dateArray);
+    echo json_encode($data);
+  }
 }
--- graphic.php
+++ graphic.php
@@ -11,75 +11,211 @@
 <body>
     <center>
         <h2 id="title"> </h2>
+
+        <button onclick="changeChartData('s')">Salinidad</button>
+        <button onclick="changeChartData('h')">Humedad</button>
+        <button onclick="changeChartData('t')">Temperatura</button>
+
+        <h1 id="dataTitle">Salinidad</h1>
+        <p>Valor más alto: <span id="hValue">1</span></p>
+        <p>Valor más bajo: <span id="lValue">1</span></p>
+
+        <form>
+            Fecha: <select id="selectDate"></select>
+        </form>     
+
+        <form>
+            Comparar con... <select id="selectComparation">
+            <option>El mes anterior</option>
+            <option>Hace 2 semanas</option>
+            <option>La semana pasada</option>
+            </select>
+        </form>  
     </center>
 
     <canvas id="grafico" width="200" height="200"></canvas>
 
     <script>
+        let chart;
+        let hData;
+        let actualType;
+        let probeID;
+        
+        function addDatesSelector(){
+            var select = document.getElementById('selectDate');
+            select.addEventListener('change', function(e) {
+
+                fetch("./api/v1/chart.php?probeID=" + probeID + "&date=" + e.target.value, {
+                }).then(function (result) {
+                    if(result.ok) return result.json();
+                }).then(function (data) {
+                    if(data != null){
+                        hData = data;
+                        changeChartData(actualType);
+                    }
+                });  
+            }, false);
+
+            var dates = hData.allDates;
+
+            for (let i = dates.length-1; i >=0 ; i--) {
+                const element = dates[i];
+                var opt = document.createElement('option');
+                opt.value = element;
+                opt.innerHTML = element;
+                select.appendChild(opt);
+            }
+
+            select.value = hData.date;
+
+        }
+
         window.addEventListener("load", function(){
             <?php
             if(!isset($_GET['id'])){
                 exit();
             }
             ?>
-            var id = "<?php echo $_GET['id']; ?>";
+            probeID = "<?php echo $_GET['id']; ?>";
 
-            var formData = new FormData();
-            formData.append('probeID', id);
-
-            fetch("./api/v1/chart.php", {
-                method: "POST",
-                body: formData
+            fetch("./api/v1/chart.php?probeID=" + probeID, {
             }).then(function (result) {
                 if(result.ok){
                     return result.json();
                 }
             }).then(function (data) {
                 if(data != null){
-
-                    createChart(data.s, data.h, data.l, data.t)
+                    hData = data;
+                    createChart(JSON.parse(data.s), JSON.parse(data.h), JSON.parse(data.t))
                     title.innerHTML = data.date;
+                    actualType = "s";
+                    addDatesSelector();
                 }
             });
         });
 
-        function createChart(sData, hData, lData, tData){
-            var label = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96]
-            new Chart(document.getElementById("grafico"), {
+        function changeChartData(type){
+            //if(actualType === type) return;
+
+            if(type === "s"){
+                chart.data.datasets[0].data = calcAverage(JSON.parse(hData.s));
+                chart.data.datasets[0].label = "Media de Salinidad";
+                chart.data.datasets[0].borderColor = "#bc9b75";
+                actualType = "s";
+                dataTitle.innerHTML = "Salinidad";
+
+            }else if(type === "h"){
+                chart.data.datasets[0].data = calcAverage(JSON.parse(hData.h));
+                chart.data.datasets[0].label = "Media de Humedad";
+                chart.data.datasets[0].borderColor = "#12b8f1";
+                actualType = "h";
+                dataTitle.innerHTML = "Humedad";
+
+            }else if(type === "t"){
+                chart.data.datasets[0].data = calcAverage(JSON.parse(hData.t));
+                chart.data.datasets[0].label = "Media de Temperatura";
+                chart.options.scales.y.suggestedMin = -10;
+                chart.options.scales.y.suggestedMax = 50;
+                chart.data.datasets[0].borderColor = "#fc3216";
+                actualType = "t";
+                dataTitle.innerHTML = "Temperatura";
+
+            }
+
+            if(type != "t"){
+                chart.options.scales.y.suggestedMin = 0;
+                chart.options.scales.y.suggestedMax = 100;
+            }
+            chart.update();
+        }
+
+        function createChart(sData, hData, tData){
+            chart = new Chart(document.getElementById("grafico"), {
                 type: 'line',
                 data: {
-                    labels: label,
-                    datasets: [{
-                        data: sData,
-                        label: "Salinidad",
-                        borderColor: "#8e5ea2",
-                        fill: false,
-                        tension: 0.1
-                    }, {
-                        data: hData,
-                        label: "Humedad",
-                        borderColor: "#3e95cd",
-                        fill: false,
-                        tension: 0.1
-                    }, {
-                        data: tData,
-                        label: "Temperatura",
-                        borderColor: "#253016",
-                        fill: false,
-                        tension: 0.1
-                    }
+                    labels: ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"],
+                    datasets: [
+                        {
+                            data: calcAverage(sData),
+                            label: "Media de Salinidad",
+                            borderColor: "#bc9b75",
+                            fill: false,
+                            tension: 0.1,
+                            pointRadius: 5,
+                            pointHoverRadius: 7
+                        }
                     ]
                 },
                 options: {
-                responsive: true,
-                scales: {
-                        y: {
-                            stacked: true,
-                            suggestedMin: 0,
-                            suggestedMax: 1,
-                        }},
-            }
+                    layout: {
+                        padding: {
+                            left: 50,
+                            right: 50,
+                            top: 0,
+                            bottom: 0
+                        }
+                    },
+                    plugins: {
+                        legend: false,
+                    },
+                    maintainAspectRatio: true,
+                    responsive: true,
+                    scales: {
+                            y: {
+                                stacked: true,
+                                suggestedMin: 0,
+                                suggestedMax: 100,
+                            }
+                        },
+                }
             });
+
+        }
+
+        function calcAverage(data){
+            var newData = []
+            var averageNum = 0;
+            
+            for (let i = 0; i < data.length; i++) {
+                const measure = data[i];
+                
+                averageNum += measure;
+                if(((i+1) % 4) == 0){
+                    newData.push(averageNum/4);
+                    averageNum = 0;
+                }  
+                
+            }
+
+            var maxV = Math.max.apply(null, data);
+            var minV = Math.min.apply(null, data);
+
+            var maxVH = data.indexOf(maxV)/4;
+            var minVH = data.indexOf(minV)/4;
+
+            hValue.innerHTML = maxV + " (" + Math.trunc(maxVH) + ":" + calcMinute(maxVH) + ")";
+            lValue.innerHTML = minV + " (" + Math.trunc(minVH) + ":" + calcMinute(minVH) + ")";
+
+            return newData;
+        }
+
+        function calcMinute(min){
+            min = (min + "").split(".");
+            var dec = min[1];
+
+            if(dec != null){
+                switch(dec){
+                    case '25':
+                        return 15;
+                    case '50':
+                        return 30;
+                    case '75':
+                        return 45;
+                }
+            }
+
+            return "00";
+            
         }
 
     </script>
