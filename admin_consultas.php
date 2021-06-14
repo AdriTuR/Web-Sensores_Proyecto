50a17190ba698ac0d4f40da6e71491b0a6453add
Sairo1234 <alejandronogbel@gmail.com>
2021-06-14 12:01:36 +0200

    Gestionar consultas admin
    Hecho el panel de gestionar las consultas del administrador

--- admin_consultas.php
+++ admin_consultas.php
@@ -11,18 +11,93 @@ function customHead(){?>
 <body>
 <section class="conjunto">
 
-<div class="espacio_canvas">
-    <canvas id="myCanvas">
-
-    </canvas>
-</div>
-
     <script>
         var removeMe = document.getElementById("consult");
         removeMe.innerHTML = '';
     </script>
 
+<h1 class="banner">CONSULTAS</h1>
 
+<!--             TABLA                   -->
+<!--  -------------------------------    -->
+<?php 
+$conn = mysqli_connect("localhost","root","","grupo9");
+if($conn-> connect_error) {
+  die("Connection failed".$conn);
+}
+$query = "SELECT id, username, role from user where role='USER'";
+$result=mysqli_query($conn,$query);
+?>
+<div class="container" id="container_lista">
+
+<div id="lista_contener">
+
+    <div id="filtroboton">
+        <form id=Cfiltro>
+            <select id="selectComparation">
+                <option>Todas las consultas</option>
+                <option>Consultas sin leer</option>
+                <option>Consultas de hoy</option>
+                <option>Usuarios no registrados</option>
+                <option>Usuarios registrados</option>
+                <option>Solicitar sensores</option>
+                <option>Modificar terrenos/parcelas</option>
+                <option>Problemas con la cuenta</option>
+                <option>Problemas con los sensores</option>
+            </select>
+        </form>
+
+        <button id="refresh">
+            <i class="fas fa-sync fa-1.5x"></i>
+        </button>
+    </div>
+
+
+<table class="table table-bordered " id="tablacons">
+    <tbody id="myTable2">
+
+        <tr>
+            <td>
+                <div class="arriba">
+                    <div id="Tnombre">
+                        Joseba Jimenez
+                    </div>
+
+                    <div id="Tfecha">
+                        09/05/2021 19:26
+                    </div>
+                </div>
+
+                <div id="Tconsulta">
+                    Buenas tardes, acabo de encontrar esta página y he leido sobre todo lo que ofreceis,
+                    estaria interesado en obtener...
+                <div>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <div class="arriba">
+                    <div id="Tnombre">
+                        Joseba Jimenez
+                    </div>
+
+                    <div id="Tfecha">
+                        09/05/2021 19:26
+                    </div>
+                </div>
+
+                <div id="Tconsulta">
+                    Buenas tardes, acabo de encontrar esta página y he leido sobre todo lo que ofreceis,
+                    estaria interesado en obtener...
+                <div>
+            </td>
+        </tr>
+        
+
+    </tbody>
+</table>
+</div>
 
 </section>
 <?php include_once './includes/footer.php';?>
