4eafe0fde4fe439876c3c427e2a04d9ab6623f36
Sairo1234 <alejandronogbel@gmail.com>
2021-06-29 16:16:50 +0200

    Usuario Consulta

--- includes/menu_user.php
+++ includes/menu_user.php
@@ -18,7 +18,7 @@
             </li>
             <li onclick="menucerrar()" id="userpanel"><a href="user_panel.php"><img id= "iconomapainteractivo" src="images/landing_page/interactive-map_icon.png" alt="icono de un mapa interactivo"></i>TERRENOS</a></li>
             <li onclick="menucerrar()" id="userprofile"><a href="user_profile.php"><i class="bi bi-person-fill"></i>PERFIL</a></li>
-            <!--<li onclick="menucerrar()" id="userconsult"><a href="user_consult.php"><i class="fas fa-clipboard"></i>CONSULTAR</a></li>----->
+            <li onclick="menucerrar()" id="userconsult"><a href="" data-toggle="modal" data-target="#consulta"><i class="fas fa-clipboard"></i>CONSULTAR</a></li>
             <li onclick="menucerrar()"><a class="btn_logout" type="button" data-toggle="modal" data-target="#panelsesion"><i class="bi bi-box-arrow-right"></i>CERRAR SESIÓN</a></li>
         </ul>
 
