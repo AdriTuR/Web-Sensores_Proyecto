b054796467725d7f62363d81ef1635b60fea5f9e
Victor Gracia <vgaresp@epsg.upv.es>
2021-06-29 23:29:02 +0200

    sidebar

--- includes/menu_user.php
+++ includes/menu_user.php
@@ -20,6 +20,8 @@
             <li onclick="menucerrar()" id="userprofile"><a href="user_profile.php"><i class="bi bi-person-fill"></i>PERFIL</a></li>
             <li onclick="menucerrar()" id="userconsult"><a href="" data-toggle="modal" data-target="#consulta"><i class="fas fa-clipboard"></i>CONSULTAR</a></li>
             <li onclick="menucerrar()"><a class="btn_logout" type="button" data-toggle="modal" data-target="#panelsesion"><i class="bi bi-box-arrow-right"></i>CERRAR SESIÓN</a></li>
+
+            <li id="usuario"><hr class="line_panel"><a><i class="fa fa-user-circle fa-2x" aria-hidden="true" ></i>USUARIO</a></li>
         </ul>
 
     </div>
@@ -32,6 +34,7 @@
             document.getElementById("menu-btn").checked = false
         },1000)
     }
+
 </script>
 <!------------------------------------------------------------------------------------------------------------------>
 <!------------------------------------------------------------------------------------------------------------------>
